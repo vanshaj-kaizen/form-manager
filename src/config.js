8e5eb4e6495972bfd9c4f7f58c03f73c520486a4
Vanshaj <vanshajkaizen@gmail.com>
2025-09-19 13:54:17 +0530

    fix

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const config = { apiUrl: process.env.REACT_APP_API_URL };
+const config = { apiUrl: 'https://form-manager-backend-cachaahrbcf2dxfx.canadacentral-01.azurewebsites.net/' };
 
 export default config;
